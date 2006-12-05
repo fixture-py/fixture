35b35c92d4995115c1d1b83a9f54149250275390
kumar mcmillan <kumar.mcmillan@27107627-ce22-0410-b88b-0b808a85e153>
2006-12-05 15:31:50 +0000

    updates to LazyLoader

--- fixture/loader.py
+++ fixture/loader.py
@@ -3,9 +3,17 @@ from fixture.exc import UninitializedError
 default = None
 
 class LazyLoader(object):
-    def __init__(self, loader=default):
+    def __init__(self, **kw):
+        if kw.has_key('loader'):
+            loader = kw['loader']
+            del kw['loader']
+        else:
+            loader = default
+        self.kw = kw
         self.loader = loader
-    def __call__(self):
+        
+    def __call__(self, **kw):
+        self.kw.update(kw)
         if self.loader is None:
-            raise UninitializedError("no default loader has been set")
-        return self.loader()
+            raise UninitializedError("fixture.loader.default has not been set")
+        return self.loader(**self.kw)
