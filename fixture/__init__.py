45b800b0183bff5a8d4c283047ec0ccb269530e9
kumar mcmillan <kumar.mcmillan@27107627-ce22-0410-b88b-0b808a85e153>
2007-02-07 03:39:22 +0000

    adding an opening example/ doctest of SequencedSets

--- fixture/__init__.py
+++ fixture/__init__.py
@@ -2,7 +2,7 @@
 """
 python module for loading and referencing test data
 
-fixture provides an interface for loading tabular data into arbitrary storage media. You create generic !DataSet objects that can be loaded into a database, CSV file, XML file, or anything else suitable.  This is useful for testing and came about to solve problems like these:
+fixture provides an interface for loading tabular data into storage media. You create generic !DataSet objects that can be loaded into a database, CSV file, or anything else suitable.  This is useful for testing and came about to solve problems like these:
 
   * Your test needs to load lots of data and you want to easily reference that data when making assertions.
   * You want to group together commonly loaded data for your tests and keep this separate from assertions.
@@ -10,6 +10,26 @@ fixture provides an interface for loading tabular data into arbitrary storage me
   * You want to reference linked rows by variable name, not hard-coded ID number.
   * In fact, you don't even want to worry about auto-incrementing sequences and you don't want to repeat column values that don't change.
   * You want to recreate an environment (say, for a bug) by using fixture code generated from a database query (see `fixture.command.generate`).
+
+
+!DataSet objects are written as python classes, without the need to type auto-incrementing IDs; referencing other data sets is simple::
+{{{
+
+    >>> class BannerData(SequencedSet):
+    ...     class free_spaceship:
+    ...         text="Get a free spaceship with any purchase"
+    ...
+    >>> class EventData(SequencedSet):
+    ...     class click:
+    ...         name="click"
+    ...         banner_id=BannerData.free_spaceship.ref('id')
+    ...     class submit(click):
+    ...         name="submit"
+    ...     class order(click):
+    ...         name="order"
+    ...
+    
+}}}
 
 Note that this module is more or less a complete rewrite of the fixtures interface first distributed in [http://testtools.python-hosting.com/ testtools].  The new interface still has room to evolve and there are probably a couple undiscovered bugs so please don't hesitate to [http://code.google.com/p/fixture/issues/list submit an issue].
 
