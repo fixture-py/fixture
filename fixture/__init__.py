56cc3dfd128def7c83e7f2b27ef82493fdb45bf5
kumar mcmillan <kumar.mcmillan@27107627-ce22-0410-b88b-0b808a85e153>
2007-03-07 19:23:06 +0000

    updated intro docs

--- fixture/__init__.py
+++ fixture/__init__.py
@@ -2,17 +2,25 @@
 """
 python module for loading and referencing test data
 
-fixture provides an interface for loading tabular data into storage media. You create generic !DataSet objects that can be loaded into a database, CSV file, or anything else suitable.  This is useful for testing and came about to solve problems like these:
+fixture provides several utilities for achieving a _fixed state_ when testing 
+python programs.  Specifically, these are designed to setup/teardown database 
+data and/or work with a temporary file system.  This is useful for testing and 
+came about to solve problems like these:
 
-  * Your test needs to load lots of data and you want to easily reference that data when making assertions.
-  * You want to group together commonly loaded data for your tests and keep this separate from assertions.
+  * Your test needs to load data into a database and you want to easily reference that data when making assertions.
   * You want data linked by foreign key to load automatically and delete without integrity error.
   * You want to reference linked rows by variable name, not hard-coded ID number.
   * In fact, you don't even want to worry about auto-incrementing sequences and you don't want to repeat column values that don't change.
-  * You want to recreate an environment (say, for a bug) by using fixture code generated from a database query (see `fixture.command.generate`).
+  * You want to recreate an environment (say, for a bug) by using real data generated from a database query (see `fixture.command.generate`).
+  * You want to work with files in a temporary file system.
 
+For more info, this concept is explained in the wikipedia article, [http://en.wikipedia.org/wiki/Test_fixture Test Fixture].
 
-!DataSet objects are written as python classes, without the need to type auto-incrementing IDs; referencing other data sets is simple::
+===DataSet Objects===
+
+To load data into a database (or anything suitable) you create subclasses of 
+`fixture.DataSet` like so:
+
 {{{
 
     >>> class BannerData(DataSet):
@@ -28,10 +36,8 @@ fixture provides an interface for loading tabular data into storage media. You c
     ...     class order(click):
     ...         name="order"
     ...
-    
+    >>> 
 }}}
-
-Note that this module is more or less a complete rewrite of the fixtures interface first distributed in [http://testtools.python-hosting.com/ testtools].  The new interface still has room to evolve and there are probably a couple undiscovered bugs so please don't hesitate to [http://code.google.com/p/fixture/issues/list submit an issue].
 
 ===Source===
 
@@ -39,17 +45,21 @@ Note that this module is more or less a complete rewrite of the fixtures interfa
 
 ===Install===
 
-from the root source directory:
+Using the [http://peak.telecommunity.com/DevCenter/EasyInstall easy_install] command:
 
-{{{
-python setup.py install
-}}}
+{{{easy_install fixture}}}
 
-or if you have the [http://peak.telecommunity.com/DevCenter/EasyInstall easy_install] command:
+Or, if you want to create a link to the source without installing anything, cd into the root directory and type:
 
-{{{
-easy_install fixture
-}}}
+{{{python setup.py develop}}}
+
+Or ... if you're old school, this works with or without [http://peak.telecommunity.com/DevCenter/setuptools setuptools]:
+
+{{{python setup.py install}}}
+
+
+
+Note that this module is more or less a complete rewrite of the fixtures interface first distributed in [http://testtools.python-hosting.com/ testtools].  The new interface still has room to evolve and there are probably a couple undiscovered bugs so please don't hesitate to [http://code.google.com/p/fixture/issues/list submit an issue].
 
 """
 
