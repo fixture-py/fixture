0183e00d576059ffcb64713a102d952d01c93dfc
kumar mcmillan <kumar.mcmillan@27107627-ce22-0410-b88b-0b808a85e153>
2008-07-15 18:57:21 +0000

    added exception API docs

--- fixture/exc.py
+++ fixture/exc.py
@@ -5,7 +5,25 @@ class UninitializedError(Exception):
     pass
 
 class DataSetActionException(Exception):
-    """an exception while performing some action with a DataSet."""
+    """
+    An exception while performing some action with a DataSet.
+    
+    In addtion to ``etype`` and ``val`` adds these properties:
+    
+    ``dataset``
+        :class:`DataSet <fixture.dataset.DataSet>` that caused the exception
+        
+    ``key``
+        Key on DataSet row if there is one
+        
+    ``row``
+        :class:`DataRow <fixture.dataset.DataRow>` if there is one
+        
+    ``stored_object``
+        Stored object if there is one
+        
+    used by :mod:`fixture.loadable` classes
+    """
     def __init__(self, etype, val, dataset, 
                         key=None, row=None, stored_object=None):
         msg = "in %s" % dataset
@@ -17,14 +35,23 @@ class DataSetActionException(Exception):
         Exception.__init__(self, "%s: %s (%s)" % (etype.__name__, val, msg))
         
 class LoadError(DataSetActionException):
-    """an exception while loading data in DataSet."""
+    """
+    An exception while loading data in DataSet.
+    
+    used by :mod:`fixture.loadable` classes
+    """
     pass
 class UnloadError(DataSetActionException):
-    """an exception while unloading data from a DataSet."""
+    """
+    An exception while unloading data from a DataSet.
+    
+    used by :mod:`fixture.loadable` classes
+    """
     pass
 
 class StorageMediaNotFound(LookupError):
-    """Looking up a storable object failed.
+    """
+    Looking up a storable object failed.
     
     used by :mod:`fixture.loadable` classes
     """
