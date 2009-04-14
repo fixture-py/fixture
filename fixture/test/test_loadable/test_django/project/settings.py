5c3f85e85babc23208a6a9d3cfc59492f50d5793
Ben Ford <ben.fordnz@gmail.com>
2009-04-14 13:24:03 +0100

    Added support for specifying the django model as a django_model attribute on the inner Meta class (this should be of the form 'app.Model'). Also added a TransactionTestCase subclass for doing the setup and teardown of fixtures (this need the DJANGO_SETTINGS_MODULE environmental variable). Buildout is not configured to need django > 1.1.0 wich will have to put into src/django per default.

--- fixture/test/test_loadable/test_django/project/settings.py
+++ fixture/test/test_loadable/test_django/project/settings.py
@@ -9,9 +9,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'project.db')             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'project.db')
+# MY POSTGRES SETTINGS
+#DATABASE_ENGINE = 'postgresql_psycopg2'
+#DATABASE_NAME = 'fixture' #
+#DATABASE_USER = 'ben'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
