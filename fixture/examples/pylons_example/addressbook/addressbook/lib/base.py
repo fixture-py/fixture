705906c49e2e6f980d07763c48b777d82ba6c035
kumar <kumar.mcmillan@gmail.com>
2009-05-21 09:17:10 -0500

    new base

--- fixture/examples/pylons_example/addressbook/addressbook/lib/base.py
+++ fixture/examples/pylons_example/addressbook/addressbook/lib/base.py
@@ -1,17 +1,10 @@
 """The base Controller API
 
-Provides the BaseController class for subclassing, and other objects
-utilized by Controllers.
+Provides the BaseController class for subclassing.
 """
-from pylons import c, cache, config, g, request, response, session
 from pylons.controllers import WSGIController
-from pylons.controllers.util import abort, etag_cache, redirect_to
-from pylons.decorators import jsonify, validate
-from pylons.i18n import _, ungettext, N_
-from pylons.templating import render
+from pylons.templating import render_mako as render
 
-import addressbook.lib.helpers as h
-import addressbook.model as model
 from addressbook.model import meta
 
 class BaseController(WSGIController):
@@ -25,8 +18,3 @@ class BaseController(WSGIController):
             return WSGIController.__call__(self, environ, start_response)
         finally:
             meta.Session.remove()
-
-
-# Include the '_' function in the public names
-__all__ = [__name for __name in locals().keys() if not __name.startswith('_') \
-           or __name == '_']
