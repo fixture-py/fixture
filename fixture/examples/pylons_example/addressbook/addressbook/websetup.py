ab8d95f063e1853216ff28d30ddd7ba429ab7419
kumar <kumar.mcmillan@gmail.com>
2009-05-21 09:32:29 -0500

    updated websetup

--- fixture/examples/pylons_example/addressbook/addressbook/websetup.py
+++ fixture/examples/pylons_example/addressbook/addressbook/websetup.py
@@ -1,27 +1,22 @@
 """Setup the addressbook application"""
 import logging
 
-from paste.deploy import appconfig
-from pylons import config
-
 from addressbook.config.environment import load_environment
-from addressbook import model
 from addressbook.model import meta
 
+from addressbook import model
 from fixture import SQLAlchemyFixture
 from fixture.style import NamedDataStyle
 from addressbook.datasets import PersonData
 
 log = logging.getLogger(__name__)
 
-def setup_config(command, filename, section, vars):
+def setup_app(command, conf, vars):
     """Place any commands to setup addressbook here"""
-    conf = appconfig('config:' + filename)
     load_environment(conf.global_conf, conf.local_conf)
-    
-    # initialize the DB :
-    
+
     log.info("Creating tables")
+    # Create the tables if they don't already exist
     meta.metadata.create_all(bind=meta.engine)
     log.info("Successfully setup")
     
