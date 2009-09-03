5d6fcc9f751b2bfb9e6be38918fca7db92b15781
kumar <kumar.mcmillan@gmail.com>
2009-09-03 10:54:05 -0500

    Adds path to django project for running ./bin/manage from buildout

--- fixture/examples/django_example/manage.py
+++ fixture/examples/django_example/manage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
