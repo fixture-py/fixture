70f902383423e688620aa43c31d791a7e84dc85a
kumar <kumar.mcmillan@gmail.com>
2009-09-12 15:47:12 -0500

    updated run_tests.sh to use buildout generated test script

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,7 @@
 source fixture/test/profile/full.sh
 # also see setup.cfg for more options
 
-pushd docs
-make doctest
-popd
+make -C docs doctest
 
-# -i includes for pylons example app
-nosetests -i source -i examples -i pylons_example -i addressbook
-
-# NoseGAE does not work in virtualenv but it would be nice of it did \
-# -i google_appengine_example \
-# --with-gae
+# this is created by bin/buildout:
+./bin/test-fixture
