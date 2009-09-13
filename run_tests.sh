7730eca0fcab097638e5fc1194b02e28693c0d46
kumar <kumar.mcmillan@gmail.com>
2009-09-12 23:38:02 -0500

    Catch exit status of make doctest in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,12 @@ source fixture/test/profile/full.sh
 
 make -C docs doctest
 
+# there is a bug in zc.buildout which needs to 
+# be fixed before make will exit non-zero
+# https://bugs.launchpad.net/zc.buildout/+bug/164629/
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
 # this is created by bin/buildout:
 ./bin/test-fixture
