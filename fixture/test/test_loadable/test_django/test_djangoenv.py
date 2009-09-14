257f38ef16523e7635ed6eb77dbaea25f1adcb1b
kumar <kumar.mcmillan@gmail.com>
2009-09-14 18:46:21 -0500

    Removed a lot of code from the Django example blog because it wasn't necessary for the purposes of the fixture example.

--- fixture/test/test_loadable/test_django/test_djangoenv.py
+++ fixture/test/test_loadable/test_django/test_djangoenv.py
@@ -1,58 +1,58 @@
 from nose.tools import raises, assert_raises, assert_equal
 from fixture.loadable.django_loadable import DjangoEnv, DjangoFixture
-from fixture.examples.django_example.app.models import Book
+from fixture.examples.django_example.blog.models import Post
 from fixture import DataSet
 from fixture.style import NamedDataStyle
 
 @raises(ValueError) 
 def test_unsplittable_name():
-    DjangoEnv.get('app_Book')
+    DjangoEnv.get('blog_Post')
     
 def test_invalid_app_returns_none():
-    assert_equal(None, DjangoEnv.get('no_such_app__Book'))
+    assert_equal(None, DjangoEnv.get('no_such_app__Post'))
     
 def test_invalid_model_returns_none():
-    assert_equal(None, DjangoEnv.get('app__NoSuchModel'))
+    assert_equal(None, DjangoEnv.get('blog__NoSuchModel'))
 
 def test_model_lookup_by_qualified_model_name():
     
     class SomeDataset(DataSet):
         class Meta:
-            django_model = "app.Book"
+            django_model = "blog.Post"
         class foo:
             foo = 1
     
     fixture = DjangoFixture()
     ds = SomeDataset()
     fixture.attach_storage_medium(ds)
-    assert_equal(ds.meta.storage_medium.medium, Book)
+    assert_equal(ds.meta.storage_medium.medium, Post)
 
 def test_model_lookup_by_explicit_app_label_and_name():
     
     class SomeDataset(DataSet):
         class Meta:
-            django_app_label = "app"
-            storable_name = "Book"
+            django_app_label = "blog"
+            storable_name = "Post"
         class foo:
             foo = 1
     
     fixture = DjangoFixture()
     ds = SomeDataset()
     fixture.attach_storage_medium(ds)
-    assert_equal(ds.meta.storage_medium.medium, Book)
+    assert_equal(ds.meta.storage_medium.medium, Post)
 
 def test_model_lookup_by_app_label_and_style_derived_name():
     
-    class BookData(DataSet):
+    class PostData(DataSet):
         class Meta:
-            django_app_label = "app"
+            django_app_label = "blog"
         class foo:
             foo = 1
     
     fixture = DjangoFixture(style=NamedDataStyle())
-    ds = BookData()
+    ds = PostData()
     fixture.attach_storage_medium(ds)
-    assert_equal(ds.meta.storage_medium.medium, Book)
+    assert_equal(ds.meta.storage_medium.medium, Post)
 
 @raises(ValueError)
 def test_dataset_with_malformed_model_name():
