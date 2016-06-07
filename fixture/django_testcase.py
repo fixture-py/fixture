9cd31a43477d577568e2953b5ae6b4ea2798b2b6
Francisco Ruiz <franciscoruiz@2degreesnetwork.com>
2016-06-07 13:37:09 +0100

    Added calls to `super` in setup/teardown in FixtureTestCase for Django

--- fixture/django_testcase.py
+++ fixture/django_testcase.py
@@ -4,14 +4,17 @@ from fixture import DjangoFixture
 
 
 class FixtureTestCase(TestCase):
+    """
+    Overrides django's fixture setup and teardown code to use DataSets.
 
-    """Overrides django's fixture setup and teardown code to use DataSets.
+    See :ref:`Using Fixture With Django <using-fixture-with-django>` for a
+    complete example.
 
-    See :ref:`Using Fixture With Django <using-fixture-with-django>` for a complete example.
     """
 
     @classmethod
     def setUpTestData(cls):
+        super(FixtureTestCase, cls).setUpTestData()
         fixture = DjangoFixture()
         cls.data = fixture.data(*cls.datasets)
         cls.data.setup()
@@ -19,3 +22,4 @@ class FixtureTestCase(TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.data.teardown()
+        super(FixtureTestCase, cls).tearDownClass()
